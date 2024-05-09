7994cf7e62d00292ca1cc43cdc33c432111b10ce
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-05-09 00:19:32 -0400

    Simplify code

--- packages/server/src/middlewares/prisma.ts
+++ packages/server/src/middlewares/prisma.ts
@@ -1,17 +1,12 @@
 import { PrismaClient } from "@pipelines/database";
-import { withAccelerate } from "@pipelines/database";
 import { Next } from "hono";
 
 import type { HonoContext } from "../../config";
 
 export function db(c: HonoContext, next: Next) {
     const prisma = new PrismaClient({
-        datasources: {
-          db: {
-            url: c.env.DATABASE_URL
-          },
-        },
-      }).$extends(withAccelerate());
+        datasourceUrl: c.env.DATABASE_URL
+      })
     c.set("db", prisma);
     return next();
 }
