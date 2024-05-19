cfdf7a4c27fbcf8d0c50637819c13256fe766745
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-05-19 11:16:08 -0400

    Deleted test router and added companyRouter to global router

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,12 @@
 import { router } from "./src/trpc";
 import { profileRouter } from "./src/routers/profile";
 import { schoolRouter } from "./src/routers/school";
+import { companyRouter } from "./src/routers/company";
 
 const appRouter = router({
   profile: profileRouter,
-  school: schoolRouter
+  school: schoolRouter,
+  company: companyRouter
 });
 
 export type AppRouter = typeof appRouter;
