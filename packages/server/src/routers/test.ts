12367b5191d47dd74223a464337218bb16a1ebfb
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-05-19 11:16:08 -0400

    Deleted test router and added companyRouter to global router

--- packages/server/src/routers/test.ts
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpc";
-import { publicProcedure } from "../trpc";
-
-export const testRouter = router({
-  hello: publicProcedure.query(async ({ ctx }) => {
-    const users = await ctx.db.profiles.findMany();
-    return users;
-  }),
-});
